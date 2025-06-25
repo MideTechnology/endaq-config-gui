505130ef025cb0ed5bfc9877d585bec975931648
stokesMIDE <dstokes@mide.com>
2025-06-25 16:52:35 -0400

    Version bump (2.0.4)

--- endaqconfig/__init__.py
+++ endaqconfig/__init__.py
@@ -3,11 +3,12 @@ A GUI for configuring enDAQ data recoders. This can be run standalone, or
 imported into another script.
 """
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
+__author__ = "David Stokes"
+__copyright__ = "Copyright 2025 Mide Technology Corporation"
 
 import logging
 
 logger = logging.getLogger('endaqconfig')
 logger.setLevel(logging.INFO)
 logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
-
